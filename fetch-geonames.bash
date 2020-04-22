f29cf261d3691a61124a6560fdaa2eb6a9ee9cca
Francis Bond <bond@ieee.org>
2020-04-22 11:32:03 +0800

    added script to get the geonames data

---
+++ fetch-geonames.bash
@@ -0,0 +1,6 @@
+pushd gndata
+wget http://download.geonames.org/export/dump/allCountries.zip	
+wget http://download.geonames.org/export/dump/alternateNamesV2.zip
+unzip allCountries.zip	
+unzip alternateNamesV2.zip
+popd
